7bd6789f93f083acc3bd5e1971a12e94daf68209
Jordan Kimsey <jordan.kimsey@pressganey.com>
2024-01-16 11:37:36 -0500

    Added publish logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/index.tsx'),
+      // The name of the package
+      name: 'PGForsta-design-system',
+      fileName: 'pgforsta-design-system',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  //dts rolls up all the types of our package
+  plugins: [react(), dts({ rollupTypes: true })],
+});
